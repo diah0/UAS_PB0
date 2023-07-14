e1a02a273421d3953773d261b5a6df2cd8f48716
dy <134118561+diah0@users.noreply.github.com>
2023-07-14 05:33:01 -0700

    Add files via upload

---
+++ Halimatus_Sadiah/src/main/java/com/mycompany/halimatus_sadiah/PenggajianKaryawan.java
@@ -0,0 +1,25 @@
+
+package com.mycompany.halimatus_sadiah;
+
+
+public class PenggajianKaryawan {
+    public static void main(String[] args) {
+        karyawan karyawan1= new karyawan();
+        karyawan1.id=111212;
+        karyawan1.nama="Halimatus Sa'diah";
+        karyawan1.gaji="Rp.8.000.000";
+        karyawan1.display();
+        
+        karyawan karyawan2=new karyawan();
+       karyawan2.id=2901911;
+        karyawan2.nama="Rey";
+        karyawan2.gaji="Rp.3.000.000";
+        karyawan2.display();
+        
+        
+        
+        
+        
+    }
+    
+}
---
+++ Halimatus_Sadiah/src/main/java/com/mycompany/halimatus_sadiah/karyawan.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.halimatus_sadiah;
+
+
+public class karyawan {
+    public int id;
+    public String nama;
+    public  String gaji;
+    
+    public void display(){
+        System.out.println("\nId karyawan : "+this.id);
+        System.out.println("Nama Karyawan : "+this.nama);
+        System.out.println("Gaji Karyawan : "+this.gaji);
+    
+        
+    }
+    
+}
